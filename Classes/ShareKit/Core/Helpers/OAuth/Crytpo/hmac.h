fd2490cbee5940ed45537efbb2d8a3cab0d6c4c0
John Arnold <john@handelabra.com>
2011-11-19 20:03:36 -0400

    Fix hmac warnings

--- Classes/ShareKit/Core/Helpers/OAuth/Crytpo/hmac.h
+++ Classes/ShareKit/Core/Helpers/OAuth/Crytpo/hmac.h
@@ -26,6 +26,8 @@
 #ifndef HMAC_H
 #define HMAC_H 1
 
-extern void hmac_sha1(const unsigned char *inText, int inTextLength, unsigned char* inKey, const unsigned int inKeyLength, unsigned char *outDigest);
+#include <stdlib.h>
+
+extern void hmac_sha1(const unsigned char *inText, size_t inTextLength, unsigned char* inKey, size_t inKeyLength, unsigned char *outDigest);
 
 #endif /* HMAC_H */
