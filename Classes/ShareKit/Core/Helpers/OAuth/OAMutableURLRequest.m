e080f9e01b4722bfcaa061791cc5244581ce0b18
Aaron London <github@postmechanical.com>
2011-12-04 15:00:29 -0700

    Clean up memory warning from analyzer.

--- Classes/ShareKit/Core/Helpers/OAuth/OAMutableURLRequest.m
+++ Classes/ShareKit/Core/Helpers/OAuth/OAMutableURLRequest.m
@@ -199,7 +199,10 @@ signatureProvider:(id<OASignatureProviding, NSObject>)aProvider
 {
     CFUUIDRef theUUID = CFUUIDCreate(NULL);
     CFStringRef string = CFUUIDCreateString(NULL, theUUID);
-    NSMakeCollectable(theUUID);
+    if (nonce != nil)
+    {
+        [nonce release];
+    }
     nonce = (NSString *)string;
 	CFRelease(theUUID);
 }
